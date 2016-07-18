236c1633ae0cf3563d8e0d051c4e898fc7eb6b29
cyberplanner <saleh.gfx@gmail.com>
2016-07-18 18:05:51 +0100

    step 7, start and restart

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require "sinatra"
+set :session_secret, 'super secret'
 
 get '/' do
    "Hello!"
+end
+
+get '/secret' do
+  "This is a secret message!"
+end
+
+get '/bart' do
+  "Bart was here!"
+end
+
+get '/test' do
+  "just testing!"
+end
+
+get '/testing' do
+  "just another test!"
 end
